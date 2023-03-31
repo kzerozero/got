b79b52a8daf78409b6e865ceb0f8ff8a9bf56a0d
Hata Reg <hatareg21@gmail.com>
2023-03-31 09:59:59 +0200

    Move urls to env variables.

--- src/services/characters.ts
+++ src/services/characters.ts
@@ -1,12 +1,12 @@
 import { IGotCharacter, IGotQuote } from "../gotTypes";
 
-const charactersBaseUrl = "https://thronesapi.com/api/v2/Characters";
-const quotesBaseUrl = "https://api.gameofthronesquotes.xyz/v1/author/";
+const BASE_URL=process.env.REACT_APP_BASE_URL;
+const BASE_URL_QUOTES=process.env.REACT_APP_BASE_URL_QUOTES;
 
 const getAllCharacters = async ():Promise<IGotCharacter[]> => {
   try {
     console.log("Service: getAllCharacters");
-    const response = await fetch(charactersBaseUrl);
+    const response = await fetch(BASE_URL!);
     if(!response.ok) {
       throw new Error("Failed to fetch characters");
     }
@@ -19,14 +19,14 @@ const getAllCharacters = async ():Promise<IGotCharacter[]> => {
 
 const getCharacterById = async (id:string):Promise<IGotCharacter> => {
   console.log("Service: getCharacterById");
-  const url = `${charactersBaseUrl}/${id}`;
+  const url = `${BASE_URL}/${id}`;
   const response = await fetch(url);
   return await response.json();
 };
 
 const getAllQuotes = async (name:string): Promise <IGotQuote[]> => {
   console.log("Service: getAllquotes");
-  const url = `${quotesBaseUrl}${name}/100`; 
+  const url = `${BASE_URL_QUOTES}/${name}/100`; 
   const response = await fetch(url);
   return await response.json();
 };
